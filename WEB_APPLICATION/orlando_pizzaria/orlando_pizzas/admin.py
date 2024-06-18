b7f9bc3a310a4896d29f1c51bbf2f9b12fb4e531
musicalScorpio <smukherjee0891@gmail.com>
2024-06-18 08:16:56 -0400

    refactor

--- WEB_APPLICATION/orlando_pizzaria/orlando_pizzas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Pizza
-from .models import Toppings
-
-# Register your models here.
-admin.site.register(Pizza)
-admin.site.register(Toppings)
--- WEB_APPLICATION/orlando_pizzaria/orlando_pizzas/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrlandoPizzasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'orlando_pizzas'
